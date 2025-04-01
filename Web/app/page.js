f7b9e48d65bc52924569e1683e5aa2051f8acb2a
dev <dev@devui-MacBookPro.local>
2025-04-01 14:31:30 +0900

    #5/Feat:사용자 수집 목적 렌더링 페이지 구현

--- Web/app/page.js
+++ Web/app/page.js
@@ -2,100 +2,64 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex flex-col">
+
+      <div className="flex flex-col justify-center items-center bg-pink-50 py-20 md:py-30 px-4 bg-linear-to-b from-purple-200 to-pink-100">
+        <p className="mt-10 text-3xl font-black">서비스 설명글</p>
+        <p className="mt-6 text-4xl font-bold">서비스 이름</p>
+        <button className="mt-10 px-10 py-3 bg-purple-600 rounded-full text-white text-lg font-extrabold">지금 신청하기</button>
+        <Image className="my-10" width={600} height={500} src={"https://picsum.photos/1200/1000"} alt={"logo"} />
+      </div>
+
+      <div className="flex flex-col justify-center items-center py-10 px-4 text-center bg-white">
+        <p className="mt-10 font-bold text-2xl/11">@@@ 서비스는</p>
+        <p className="font-bold text-2xl/11">점주님과 고객님 모두 설레는 <br className="block md:hidden" />시간을 만들어 가겠습니다.</p>
+        <p className="mt-4 md:mt-0 font-bold text-2xl/11">이제껏 경험 못 했던 예약 서비스로 <br className="block md:hidden" />우리의 일상은 새로워집니다.</p>
+
+        <Image className="my-10" width={400} height={400} src={"https://picsum.photos/1000"} alt={"test"} />
+      </div>
+
+      <div className="flex flex-col justify-center items-center py-10 px-4 text-center bg-white">
+        <p className="font-bold text-2xl/11">@@@ 서비스는</p>
+        <p className="font-bold text-2xl/11">가장 유연한 예약관리를</p>
+        <p className="font-bold text-2xl/11">제공합니다.</p>
+
+        <Image className="my-10" width={400} height={600} src={"https://picsum.photos/800/1200"} alt={"스크린샷"} />
+      </div>
+
+      <div className="flex flex-col justify-center items-center py-10 px-4 text-center bg-white">
+        <div className="mt-4">
+          <p className="text-3xl font-black">인스타 하세요?</p>
+          <p className="mt-4 text-gray-600 md:text-lg">인스타 프로필에 예약 페이지로</p>
+          <p className="text-gray-600 md:text-lg"> 바로 연결할 수 있는 URL을 제공해 드려요.</p>
+          <Image className="my-10" width={300} height={300} src={"https://picsum.photos/500"} alt={"스크린샷"} />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="mt-4">
+          <p className="text-3xl font-black">인스타 하세요?</p>
+          <p className="mt-4 text-gray-600 md:text-lg">인스타 프로필에 예약 페이지로</p>
+          <p className="text-gray-600 md:text-lg"> 바로 연결할 수 있는 URL을 제공해 드려요.</p>
+          <Image className="my-10" width={300} height={300} src={"https://picsum.photos/550"} alt={"스크린샷"} />
+        </div>
+
+        <div className="mt-4">
+          <p className="text-3xl font-black">인스타 하세요?</p>
+          <p className="mt-4 text-gray-600 md:text-lg">인스타 프로필에 예약 페이지로</p>
+          <p className="text-gray-600 md:text-lg"> 바로 연결할 수 있는 URL을 제공해 드려요.</p>
+          <Image className="my-10" width={300} height={300} src={"https://picsum.photos/600"} alt={"스크린샷"} />
+        </div>
+      </div>
+
+      <div className="flex flex-col mt-8 py-10 pl-14 text-start bg-slate-50">
+        <p className="font-bold leading-7">서비스명</p>
+        <p className="leading-7">위치</p>
+        <p className="leading-7">사업자등록번호</p>
+        <p className="leading-7">통신판매업 신고번호</p>
+        <p className="leading-7">제휴문의</p>
+
+      </div>
+
     </div>
   );
 }
